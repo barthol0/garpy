6360fc109ec8b82466eeaedf9599c0c963e37069
Bart Bialek <bart.bialek@protonmail.com>
2020-04-24 16:30:34 +0100

    Init.

---
+++ app.py
@@ -0,0 +1,31 @@
+import RPi.GPIO as GPIO
+import schedule
+import time
+import datetime
+
+
+relay_pin = 23
+
+GPIO.setmode(GPIO.BCM)
+GPIO.setup(relay_pin, GPIO.OUT)
+GPIO.output(relay_pin, 1)
+
+def lights_on():
+    GPIO.output(relay_pin, 0)
+
+def lights_off():
+    GPIO.output(relay_pin, 1)
+
+schedule.every().day.at("06:00").do(lights_on)
+schedule.every().day.at("22:00").do(lights_off)
+
+try:
+    if datetime.datetime.now().hour > 6:
+        lights_on()
+
+    while True:
+        schedule.run_pending()
+        time.sleep(1)
+except KeyboardInterrupt:
+    pass
+GPIO.cleanup()
